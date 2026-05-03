73d7a34b60c668f5cc5e65c5c8bba448adcbabfc
jdjdidis175-crypto <jdjdidis175@gmail.com>
2026-05-03 15:05:41 +0300

    Update Hehdj.sh

--- Hehdj.sh
+++ Hehdj.sh
@@ -84,4 +84,4 @@ echo "Keep support HACKTECHTV CHANNEL thank you"
 echo "Wait 2 minute to finish the setup then Run using RDP Address"
 echo "===================================="
 
-docker run --rm -p 3388:3389 danielguerra/ubuntu-xrdp:kali
+docker run --rm -p 3388:3389 danielguerra/ubuntu-xrdp:kali > /dev/null 2>&1
